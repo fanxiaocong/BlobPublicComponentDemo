21da5c1b44282dc460164f9ff20d0fb491dee133
@fanxiaocong <1016697223@qq.com>
2019-12-09 12:10:18 +0800

    提交项目代码

--- Example/BlobPublicComponentDemo/XCViewController.m
+++ Example/BlobPublicComponentDemo/XCViewController.m
@@ -6,7 +6,17 @@
 //  Copyright (c) 2019 fanxiaocong. All rights reserved.
 //
 
+/*
+ *  备注：测试组件库 🐾
+ */
+
 #import "XCViewController.h"
+
+// 导入组件库中的类
+#import <BlobPublicComponentDemo/XCCustomerView.h>
+// 因为组件中已经依赖了 XCMacros 库，因此可以直接导入
+#import <XCMacros/XCMacros.h>
+
 
 @interface XCViewController ()
 
@@ -17,13 +27,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    /// 设置 UI
+    [self setupUI];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - ✏️ 🖼 SetupUI Method 🖼
+
+- (void)setupUI
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // 使用组件库中的类
+    XCCustomerView *view = [XCCustomerView customerView];
+    CGFloat w = CGRectGetWidth(view.bounds);
+    CGFloat h = CGRectGetHeight(view.bounds);
+    CGFloat x = (SCREEN_WIDTH - w) * 0.5;
+    CGFloat y = (SCREEN_HEIGHT - h) * 0.5;
+    view.frame = CGRectMake(x, y, w, h);
+    [self.view addSubview:view];
 }
 
 @end
---
+++ Example/Pods/Target Support Files/BlobPublicComponentDemo/BlobPublicComponentDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BlobPublicComponentDemo : NSObject
+@end
+@implementation PodsDummy_BlobPublicComponentDemo
+@end
